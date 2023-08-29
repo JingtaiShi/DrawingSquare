bdcfd4753f7d4ad4fc54e58c9f5e4ea1bb2fe839
JingtaiShi <113505006+JingtaiShi@users.noreply.github.com>
2023-08-29 18:58:37 +1000

    create project
    add some common utils and configrations

---
+++ drawingSquare/src/main/java/com/elec5619/drawingsquare/common/status/StatusCode.java
@@ -0,0 +1,16 @@
+package com.elec5619.drawingsquare.common.status;
+
+/**
+ * 返回的状态码定义
+ * @author Rocky
+ * @create 2023-08-29
+ */
+public class StatusCode {
+    //返回成功
+    public static final int SUCCESS_CODE = 200;
+    //错误状态码
+    public static final int ERROR_CODE = 500;
+    //无权限
+    public static final int NO_LOGIN = 600;
+    public static final int NO_AUTH = 700;
+}
