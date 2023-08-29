a61512ef316a72f6d430f2308328a0f3eef706db
jingtai <jingtaishi0618@gmail.com>
2023-08-29 19:23:54 +1000

    create

---
+++ src/main/java/com/elec5619/drawingsquare/DrawingSquareApplication.java
@@ -0,0 +1,13 @@
+package com.elec5619.drawingsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawingSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elec5619/drawingsquare/DrawingSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elec5619.drawingsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
